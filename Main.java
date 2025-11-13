4953ca9c2891830f0d1ba33b97aba230fe875daf
xandao.dev <alexandrevitaldp@gmail.com>
2025-11-13 16:52:38 -0300

    teste de calcula da distancia na main

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
 public class Main {
+    public static void main(String[] args){
+        Cidade cidade1 = new Cidade(1, 1.0,5.0);
+        Cidade cidade2 = new Cidade(3,7.0,5.0);
+
+        double distancia  = cidade1.calcularDistanciaEuclidiana(cidade2);
+
+        System.out.println("distancia entre a cidade 1 e a cidade 2 é " + distancia);
+    }
     
 }
