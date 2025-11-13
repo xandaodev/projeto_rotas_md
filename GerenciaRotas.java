65c3a0bde391f996e5d4a1daab956d55b78a9231
xandao.dev <alexandrevitaldp@gmail.com>
2025-11-13 17:13:27 -0300

    adicionei a classe GerenciaRotas.java

---
+++ GerenciaRotas.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class GerenciaRotas {
+
+    private List<Cidade> cidades;
+
+    public GerenciaRotas(){
+        this.cidades = new ArrayList<>();
+    }
+
+    public List<Cidade> getCidades(){
+        return this.cidades;
+    }
+
+    public void lerArquivoDeEntrada(String entrada){
+        
+    }
+    
+}
--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
         Cidade cidade1 = new Cidade(1, 1.0,5.0);
         Cidade cidade2 = new Cidade(3,7.0,5.0);
 
-        double distancia  = cidade1.calcularDistanciaEuclidiana(cidade2);
+        double distancia  =cidade1.calcularDistanciaEuclidiana(cidade2);
 
         System.out.println("distancia entre a cidade 1 e a cidade 2 é " + distancia);
     }
