c937924942291d4ff8246b70c8dc4e25db637c12
xandao.dev <alexandrevitaldp@gmail.com>
2025-11-14 12:42:16 -0300

    adicionei a funcao de ler aqrquivo de entrada

--- GerenciaRotas.java
+++ GerenciaRotas.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class GerenciaRotas {
-
     private List<Cidade> cidades;
 
     public GerenciaRotas(){
@@ -13,7 +12,27 @@ public class GerenciaRotas {
         return this.cidades;
     }
 
-    public void lerArquivoDeEntrada(String entrada){
+    public void lerArquivoEntrada(String entrada){
+        this.cidades.clear();
+
+        // aqui tem que ter o try ctach pra evitar bo
+        try(Scanner scanner = new Scanner(new File(entrada))){
+
+            // le a primeira linha (15)
+            int numCidades =scanner.nextInt();
+            for(int i=1;i <= numCidades;i++){
+                double x = scanner.nextDouble();
+                double y = scanner.nextDouble();
+                Cidade novaCidade = new Cidade(i, x, y);
+                this.cidades.add(novaCidade);
+            }
+
+            System.out.println("arquivo " + entrada + " lido com sucesso. " + this.cidades.size() + " cidades carregadas.");
+
+        }catch(FileNotFoundException e){
+            System.err.println("erro, arquivo '" + entrada + "' nao encontrado.");
+            e.printStackTrace();
+        }
         
     }
     
